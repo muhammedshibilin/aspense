84cd351dec29904c9bd09a9cde74849afdb7cfd8
ziblyy <nkshibili17@gmail.com>
2024-03-12 06:43:14 +0530

    everything is okay

---
+++ model/cartModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+        ref: "users"
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+                required: true,
+                ref: "product"
+            }, count: {
+                type: Number,
+                default: 1
+            }, price: {
+                type: Number,
+                required: true
+            }, totalPrice: {
+                type: Number,
+                default: 0
+            }
+        }],couponDiscount:{
+            type:String,
+            ref:'coupon'
+        },shippingMethod:{
+            type:String,
+            default:"free-shipping"
+        },shippingAmount:{
+            type:Number,
+            default:0
+        }
+})
+
+module.exports = mongoose.model("cart",cartSchema)
